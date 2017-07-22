4d55059e2a076d93621ea984b80a1056e966667c
P.N.Vamshi <pnvamshi9@gmail.com>
2017-07-22 13:42:33 +0530

    Add files via upload

---
+++ EmotionAnalysis/src/signals/EEG/acquisition/EEG_aqn_variable.m
@@ -0,0 +1,32 @@
+
+%> @file EEG_aqn_variable.m
+%> @brief  EEG_aqn_variable gets an EEG signal from a variable.
+% 
+%> @param   eegChannels [Cxstrings]: a vector specifying the C EEG channels given
+%> @param   eegData [CxN]: a matrix containing the EEG data
+%> @param   sampRate [1x1]: the sampling rate, in Hz
+% 
+%> @retval   Signal: An EEG TEAP signal
+function Signal = EEG_aqn_variable(eegChannels, eegData, sampRate)
+
+if(nargin ~= 3)
+	error('Usage: Signal = EEG_aqn_variable(eegChannels, eegData, sampRate');
+end
+
+
+Signal = EEG__new_empty();
+Signal = Signal__set_samprate(Signal, sampRate);
+
+nChannels = length(eegChannels);
+[nChannelsGiven, ~] = size(eegData);
+
+if(nChannelsGiven ~= nChannels)
+	error(['Hey, you said that you''d give me ' num2str(nChannels) ...
+	       'channels but you gave me ' num2str(nChannelsGiven) ' instead']);
+end
+
+for(i = [1:nChannels])
+	chaname = strtrim(eegChannels{i});
+	Signal = EEG_set_channel(Signal, chaname, eegData(i, :));
+end
+
